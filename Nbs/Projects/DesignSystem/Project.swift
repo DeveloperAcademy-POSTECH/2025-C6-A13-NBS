b79a56111937549e2d4e6e2087a3ddec3aeb8855
Hong <dbsghdz1@gmail.com>
2025-10-10 15:37:04 +0900

    feat: 폰트 추가

--- Nbs/Projects/DesignSystem/Project.swift
+++ Nbs/Projects/DesignSystem/Project.swift
@@ -14,10 +14,17 @@ let protject = Project.project(
     Target.target(
       name: Module.DesignSystem.rawValue,
       product: .staticFramework,
+      infoPlist: .extendingDefault(with: [
+        "UIAppFonts": .array([
+          .string("Pretendard-Regular.otf"),
+          .string("Pretendard-Bold.otf"),
+          .string("Pretendard-SemiBold.otf"),
+          .string("Pretendard-Medium.otf")
+        ])
+      ]),
       sources: .sources,
       resources: .default,
-      dependencies: [
-      ]
+      dependencies: []
     )
   ]
 )
