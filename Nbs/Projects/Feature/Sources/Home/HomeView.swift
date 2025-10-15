d9b395b02f6edad7c18a8725dba5009037b2c1cb
Hong <dbsghdz1@gmail.com>
2025-10-15 21:09:05 +0900

    fix: 기사Card수정

--- Nbs/Projects/Feature/Sources/Home/HomeView.swift
+++ Nbs/Projects/Feature/Sources/Home/HomeView.swift
@@ -7,21 +7,42 @@
 
 import SwiftUI
 
+import Domain
+
 public struct HomeView {
-  
+  public init() {}
 }
 
 extension HomeView: View {
   public var body: some View {
     List {
       Section {
-        
-      }
-      
-      Section {
         HStack {
           Text("최근 추가한 링크")
           Text("더보기")
+        }
+      }
+      
+      Section {
+        ArticleCard(
+          article: Article(
+            id: UUID(),
+            url: nil,
+            imageURL: nil,
+            title: "하이",
+            createAt: .now,
+            newsCompany: "동아일보"
+          )
+        )
+      } header: {
+        HStack(spacing: 0) {
+          Text("최근 추가한 링크")
+            .font(.B1_SB)
+            .foregroundStyle(.caption1)
+          Spacer()
+          Text("더보기")
+            .font(.B2_M)
+            .foregroundStyle(.caption1)
         }
       }
     }
