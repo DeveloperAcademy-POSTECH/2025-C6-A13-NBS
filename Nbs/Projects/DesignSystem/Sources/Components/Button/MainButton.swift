36a5a22b559b3e7dc91ffab854efe13491428455
Seunge E <sjin3687@gmail.com>
2025-10-15 15:06:52 +0900

    [#3] comment: 폴더 구조

---
+++ Nbs/Projects/DesignSystem/Sources/Components/Button/MainButton.swift
@@ -0,0 +1,18 @@
+//
+//  MainButton.swift
+//  DesignSystem
+//
+//  Created by 이안 on 10/15/25.
+//
+
+import SwiftUI
+
+struct MainButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainButton()
+}
