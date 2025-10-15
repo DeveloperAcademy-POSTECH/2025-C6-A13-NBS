95ef8e52f87a2d291b8523d7648849467c74ab22
Seunge E <sjin3687@gmail.com>
2025-10-15 15:06:52 +0900

    [#3] comment: 폴더 구조

---
+++ Nbs/Projects/DesignSystem/Sources/Components/Sheet/Action/ActionBottomSheet.swift
@@ -0,0 +1,18 @@
+//
+//  ActionBottomSheet.swift
+//  DesignSystem
+//
+//  Created by 이안 on 10/15/25.
+//
+
+import SwiftUI
+
+struct ActionBottomSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActionBottomSheet()
+}
---
+++ Nbs/Projects/DesignSystem/Sources/Components/Sheet/Action/ActionBottomSheetItem.swift
@@ -0,0 +1,18 @@
+//
+//  ActionBottomSheetItem.swift
+//  DesignSystem
+//
+//  Created by 이안 on 10/15/25.
+//
+
+import SwiftUI
+
+struct ActionBottomSheetItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActionBottomSheetItem()
+}
