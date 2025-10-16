c546d806dc2de3ad9f806a7ee274ef6e3ce4dd41
YeoSeongil <seongil_yeo@naver.com>
2025-10-16 19:23:00 +0900

    [#13] feat: HighlightItem 모델 구현
    하이라이팅 정보를 가지고 있는 HighlightItem Model

---
+++ Nbs/Projects/Domain/Sources/SwiftDataModel/HighlightItem.swift
@@ -0,0 +1,67 @@
+//
+//  HighlightItem.swift
+//  Domain
+//
+//  Created by 여성일 on 10/16/25.
+//
+
+import Foundation
+import SwiftData
+
+public struct Comment: Codable, Hashable {
+    public let id: Double // 메모를 구분하기 위한 고유 ID
+    public let type: String // 메모의 종류 (What/Why/Detail)
+    public let text: String // 코멘트 내용
+    
+    public init(id: Double, type: String, text: String) {
+        self.id = id
+        self.type = type
+        self.text = text
+    }
+}
+
+@Model
+public final class HighlightItem {
+  @Attribute(.unique) public var id: String
+  public var sentence: String // 하이라이팅 문장
+  public var type: String // 하이라이팅 타입 (What/Why/Detail)
+  public var createAt: Date // 하이라이트 생성 날짜
+  public var userMemo: String = "" // 추가 메모
+  
+  @Attribute(originalName: "comments_json") private var commentsJSON: String = "[]" // JSON Comment
+  
+  // 하이라이팅 문장의 코멘트
+  public var comments: [Comment] {
+    get {
+      guard let data = commentsJSON.data(using: .utf8),
+            let decodedComments = try? JSONDecoder().decode([Comment].self, from: data) else {
+        return []
+      }
+      return decodedComments
+    }
+    set {
+      guard let data = try? JSONEncoder().encode(newValue),
+            let jsonString = String(data: data, encoding: .utf8) else {
+        commentsJSON = "[]"
+        return
+      }
+      commentsJSON = jsonString
+    }
+  }
+  
+  public var link: LinkItem? // 어떤 linkItem에 속해있는지 구분을 위한 구분자
+  
+  public init(
+    id: String,
+    sentence: String,
+    type: String,
+    createAt: Date,
+    comments: [Comment] = []
+  ) {
+    self.id = id
+    self.sentence = sentence
+    self.type = type
+    self.createAt = createAt
+    self.comments = comments
+  }
+}
