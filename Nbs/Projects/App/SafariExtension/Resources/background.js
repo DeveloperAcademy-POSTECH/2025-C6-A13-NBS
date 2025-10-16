d37eebe484b203c579d78837043dfd5663d6bf5a
YeoSeongil <seongil_yeo@naver.com>
2025-10-17 04:35:31 +0900

    [#10] feat: 사파리와 앱 백그라운드 통신 구현

--- Nbs/Projects/App/SafariExtension/Resources/background.js
+++ Nbs/Projects/App/SafariExtension/Resources/background.js
@@ -1,6 +1,21 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-
-    if (request.greeting === "hello")
-        return Promise.resolve({ farewell: "goodbye" });
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "getLatestDataForURL") {
+    console.log("background.js: content.js로부터 메시지 수신, 네이티브 코드로 전달합니다.", message);
+    
+    browser.runtime.sendNativeMessage("com.yeo.C6-Safari", message)
+    .then(response => {
+      console.log("background.js: 네이티브로부터 받은 전체 응답:", response);
+      console.log("background.js: 실제 데이터를 content.js로 전달합니다:", response);
+      sendResponse(response);
+    })
+    .catch(error => {
+      console.error("background.js: 네이티브 메시지 전송 오류:", error);
+      sendResponse({ error: error.message });
+    });
+    
+    return true;
+  }
+  
+  return false;
 });
+
