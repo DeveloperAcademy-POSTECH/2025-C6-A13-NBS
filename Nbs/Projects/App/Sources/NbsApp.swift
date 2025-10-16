eff5d506b442542b44736b8ed122658101cf6304
YeoSeongil <seongil_yeo@naver.com>
2025-10-17 04:45:05 +0900

    [#10] feat: ModelContainer 주입

--- Nbs/Projects/App/Sources/NbsApp.swift
+++ Nbs/Projects/App/Sources/NbsApp.swift
@@ -1,12 +1,37 @@
 import SwiftUI
 
+import SwiftData
+
+import Domain
 import Feature
 
 @main
 struct NbsApp: App {
+  var sharedModelContainer: ModelContainer = {
+    let schema = Schema([
+      LinkItem.self,
+      HighlightItem.self,
+      Category.self
+    ])
+    
+    let appGroupID = "group.com.nbs.dev.ADA.shared"
+    guard let containerURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroupID) else {
+      fatalError("Could not access App Group container.")
+    }
+    let storeURL = containerURL.appendingPathComponent("Nbs_store.sqlite")
+    let configuration = ModelConfiguration(schema: schema, url: storeURL)
+    
+    do {
+      return try ModelContainer(for: schema, configurations: [configuration])
+    } catch {
+      fatalError("Could not create shared ModelContainer: \(error)")
+    }
+  }()
+  
   var body: some Scene {
     WindowGroup {
       ContentView()
     }
+    .modelContainer(sharedModelContainer)
   }
 }
