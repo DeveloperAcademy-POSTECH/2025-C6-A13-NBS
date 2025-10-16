ad967f86f7805fea154a7e45842f5f477b61f071
YeoSeongil <seongil_yeo@naver.com>
2025-10-16 19:22:30 +0900

    [#13] feat: LinkItem 모델 구현
    기사 정보를 가지고 있는 LinkItem Model

---
+++ Nbs/Projects/Domain/Sources/SwiftDataModel/LinkItem.swift
@@ -0,0 +1,28 @@
+//
+//  LinkItem.swift
+//  Domain
+//
+//  Created by 여성일 on 10/16/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+public final class LinkItem {
+  @Attribute(.unique) public var id: String
+  public var urlString: String // URL
+  public var title: String // 기사 제목
+  public var createAt: Date // 링크 저장 날짜
+  @Relationship(deleteRule: .cascade) public var highlights: [HighlightItem] = [] // 해당 링크에 연결 된 하이라이트
+  
+  public init(
+    urlString: String,
+    title: String
+  ) {
+    self.id = UUID().uuidString
+    self.urlString = urlString
+    self.title = title
+    self.createAt = Date()
+  }
+}
