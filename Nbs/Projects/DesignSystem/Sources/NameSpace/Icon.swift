79bbdc2be20cebeb9ba9310828e1345ddf8e87ae
Hong <dbsghdz1@gmail.com>
2025-10-16 20:25:34 +0900

    [#1] feat: 아이콘 추가

---
+++ Nbs/Projects/DesignSystem/Sources/NameSpace/Icon.swift
@@ -0,0 +1,35 @@
+//
+//  Icon.swift
+//  DesignSystem
+//
+//  Created by 홍 on 10/16/25.
+//
+
+enum Icon {}
+
+extension Icon {
+  static let chevronLeft = "chevron-left"
+  static let chevronRight = "chevron-right"
+  static let arrowUp = "arrow-up"
+  static let plus = "plus"
+  static let x = "x"
+  static let search = "search"
+  static let settings = "settings"
+  static let moreVertical = "more-vertical"
+  static let move = "move"
+  static let trash = "trash-2"
+  static let sahre = "shared"
+  static let edit = "edit"
+  static let calendar = "calendar"
+  static let book = "book"
+  static let tag = "tag"
+  static let check = "check"
+  static let alertCircle = "alert-circle"
+  static let helpCircle = "help-circle"
+  static let info = "info"
+  static let heart = "heart"
+  static let smallxCircleFilled = "small-x-circle-filled"
+  static let smallChevronRight = "small-chevron-right"
+  static let smallChevronDown = "small-chevron-down"
+  static let smallPlus = "small-plus"
+}
