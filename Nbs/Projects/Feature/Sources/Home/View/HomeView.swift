89311a114f4f6fb0cac84cbddce03f9df5dd1f29
Hong <dbsghdz1@gmail.com>
2025-10-18 17:39:51 +0900

    [#19] feat: 주소 복사시 토스트 알림

--- Nbs/Projects/Feature/Sources/Home/View/HomeView.swift
+++ Nbs/Projects/Feature/Sources/Home/View/HomeView.swift
@@ -13,46 +13,65 @@ import DesignSystem
 
 struct HomeView {
   let store: StoreOf<HomeFeature>
+  @Environment(\.scenePhase) private var scenePhase
 }
 
 extension HomeView: View {
   var body: some View {
     NavigationStack {
       TopAppBarHome()
-      ZStack(alignment: .bottomTrailing) {
-        ScrollView {
-          VStack(spacing: 24) {
-            CategoryListView(
-              store: store.scope(
-                state: \.categoryList,
-                action: \.categoryList
+      ZStack(alignment: .bottom) {
+        ZStack(alignment: .bottomTrailing) {
+          ScrollView {
+            VStack(spacing: 24) {
+              CategoryListView(
+                store: store.scope(
+                  state: \.categoryList,
+                  action: \.categoryList
+                )
               )
-            )
-            
-            ArticleListView(
-              store: store.scope(
-                state: \.articleList,
-                action: \.articleList
+              
+              ArticleListView(
+                store: store.scope(
+                  state: \.articleList,
+                  action: \.articleList
+                )
               )
-            )
+            }
+            .padding(.bottom, 80)
           }
-          .padding(.bottom, 80)
+          
+          Button {
+            
+          } label: {
+            Image(icon: Icon.plus)
+              .foregroundStyle(DesignSystemAsset.bl6.swiftUIColor)
+              .frame(width: 48, height: 48)
+              .background(.n0)
+              .clipShape(Circle())
+          }
+          .padding(.trailing, 20)
+          .padding(.bottom, 24)
         }
         
-        Button {
-          
-        } label: {
-          Image(icon: Icon.plus)
-            .foregroundStyle(DesignSystemAsset.bl6.swiftUIColor)
-            .frame(width: 48, height: 48)
-            .background(.n0)
-            .clipShape(Circle())
+        if let alertBanner = store.state.alertBanner {
+          AlertBanner(
+            text: alertBanner.text,
+            message: alertBanner.message,
+            style: .close {
+              store.send(.dismissAlertBanner)
+            }
+          )
+          .padding(.horizontal, 24)
         }
-        .padding(.trailing, 20)
-        .padding(.bottom, 24)
       }
       .background(DesignSystemAsset.background.swiftUIColor.ignoresSafeArea())
       .navigationBarHidden(true)
+    }
+    .onChange(of: scenePhase) { _, newPhase in
+      if newPhase == .active {
+        store.send(.onAppear)
+      }
     }
   }
 }
