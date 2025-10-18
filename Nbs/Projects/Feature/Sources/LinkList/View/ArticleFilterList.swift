f4b76456c64e5cfeab85c773b5faf41bf2eea05c
Seunge E <sjin3687@gmail.com>
2025-10-19 00:46:43 +0900

    [#24] feat: ArticleFilterList 구현
    LinkListView

---
+++ Nbs/Projects/Feature/Sources/LinkList/View/ArticleFilterList.swift
@@ -0,0 +1,94 @@
+//
+//  ArticleFilterList.swift
+//  Feature
+//
+//  Created by 이안 on 10/18/25.
+//
+
+import SwiftUI
+import ComposableArchitecture
+import DesignSystem
+
+struct ArticleFilterList {
+  let store: StoreOf<ArticleFilterFeature>
+}
+
+// MARK: - Body
+extension ArticleFilterList: View {
+  var body: some View {
+    ScrollView {
+      VStack(spacing: 12) {
+        infoContents
+        articleList
+      }
+    }
+  }
+  
+  private var infoContents: some View {
+    HStack(spacing: .zero) {
+      HStack(spacing: 0) {
+            Text("총 ")
+              .font(.B1_M)
+              .foregroundStyle(.caption1)
+
+            Text("\(store.articles.count)")
+              .font(.B1_SB)
+              .foregroundStyle(.caption1)
+
+            Text("개")
+              .font(.B1_M)
+              .foregroundStyle(.caption1)
+          }
+      Spacer()
+      
+      buttonContents
+    }
+    .padding(.horizontal, 20)
+  }
+  
+  private var buttonContents: some View {
+    HStack(spacing: 5) {
+      Button {
+        store.send(.sortOrderChanged(.oldest))
+      } label: {
+        Text("오래된순")
+          .font(store.sortOrder == .oldest ? .B2_M : .C2)
+          .foregroundStyle(
+            store.sortOrder == .oldest ? .caption1 : .caption2
+          )
+      }
+      
+      Rectangle()
+        .fill(.divider2)
+        .frame(width: 1, height: 15)
+      
+      Button {
+        store.send(.sortOrderChanged(.latest))
+      } label: {
+        Text("최신순")
+          .font(store.sortOrder == .latest ? .B2_M : .C2)
+          .foregroundStyle(
+            store.sortOrder == .latest ? .caption1 : .caption2
+          )
+      }
+    }
+  }
+  
+  private var articleList: some View {
+    ForEach(store.articles) { article in
+      Button {
+        store.send(.listCellTapped)
+      } label: {
+        ArticleCard(article: article)
+      }
+    }
+  }
+}
+
+#Preview {
+  ArticleFilterList(
+    store: Store(initialState: ArticleFilterFeature.State()) {
+      ArticleFilterFeature()
+    }
+  )
+}
