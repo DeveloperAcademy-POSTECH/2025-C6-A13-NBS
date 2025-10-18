b42f60851352ad752e81cf8a1501d4158e3c3631
Hong <dbsghdz1@gmail.com>
2025-10-18 16:31:39 +0900

    [#19] 홈 화면 플로팅 추가

--- Nbs/Projects/Feature/Sources/Home/View/HomeView.swift
+++ Nbs/Projects/Feature/Sources/Home/View/HomeView.swift
@@ -19,23 +19,39 @@ extension HomeView: View {
   var body: some View {
     NavigationStack {
       TopAppBarHome()
-      ScrollView {
-        VStack(spacing: 24) {
-          CategoryListView(
-            store: store.scope(
-              state: \.categoryList,
-              action: \.categoryList
+      ZStack(alignment: .bottomTrailing) {
+        ScrollView {
+          VStack(spacing: 24) {
+            CategoryListView(
+              store: store.scope(
+                state: \.categoryList,
+                action: \.categoryList
+              )
             )
-          )
-          
-          ArticleListView(
-            store: store.scope(
-              state: \.articleList,
-              action: \.articleList
+            
+            ArticleListView(
+              store: store.scope(
+                state: \.articleList,
+                action: \.articleList
+              )
             )
-          )
+          }
+          .padding(.bottom, 80)
         }
+        
+        Button {
+          
+        } label: {
+          Image(icon: Icon.plus)
+            .foregroundStyle(DesignSystemAsset.bl6.swiftUIColor)
+            .frame(width: 48, height: 48)
+            .background(.n0)
+            .clipShape(Circle())
+        }
+        .padding(.trailing, 20)
+        .padding(.bottom, 24)
       }
+      .background(DesignSystemAsset.background.swiftUIColor.ignoresSafeArea())
       .navigationBarHidden(true)
     }
   }
