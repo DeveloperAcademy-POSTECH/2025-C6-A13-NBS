badc0b1253c582be49971d27a66e3fe359f3a7cc
YeoSeongil <seongil_yeo@naver.com>
2025-10-19 04:51:21 +0900

    [#10] fix: 카테고리 모델 이름 수정

--- Nbs/Projects/Domain/Sources/SwiftDataModel/CategoryItem.swift
+++ Nbs/Projects/Domain/Sources/SwiftDataModel/CategoryItem.swift
@@ -8,15 +8,32 @@
 import Foundation
 import SwiftData
 
+public struct CategoryIcon: Codable, Hashable {
+  public let number: Int
+  
+  public init(number: Int = 1) {
+    self.number = number
+  }
+  
+  public var name: String {
+    "CateogryIcon\(self.number)"
+  }
+}
+
 @Model
-public final class Category {
+public final class CategoryItem {
   @Attribute(.unique) public var categoryName: String // 카테고리 이름
   public var createdAt: Date
+  public var icon: CategoryIcon
   
   @Relationship(inverse: \LinkItem.category) public var links: [LinkItem] = []
   
-  public init(categoryName: String) {
+  public init(
+    categoryName: String,
+    icon: CategoryIcon
+  ) {
     self.categoryName = categoryName
     self.createdAt = Date()
+    self.icon = icon
   }
 }
