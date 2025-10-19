55823a5c28b251f7be4ea0491ae2df8703065d06
Hong <dbsghdz1@gmail.com>
2025-10-19 21:46:42 +0900

    [#26] fix: 홈 뷰 색상 수정

--- Nbs/Projects/Feature/Sources/Home/View/HomeView.swift
+++ Nbs/Projects/Feature/Sources/Home/View/HomeView.swift
@@ -19,48 +19,50 @@ struct HomeView {
 extension HomeView: View {
   var body: some View {
     NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
-      TopAppBarHome(
-        onTapSearchButton: { print ("")} ,
-        onTapSettingButton: { print ("")}
-      )
-      ZStack(alignment: .bottom) {
-        ZStack(alignment: .bottomTrailing) {
-          ScrollView {
-            VStack(spacing: 24) {
-              CategoryListView(
-                store: store.scope(
-                  state: \.categoryList,
-                  action: \.categoryList
+      VStack {
+        TopAppBarHome(
+          onTapSearchButton: { print ("")} ,
+          onTapSettingButton: { print ("")}
+        )
+        ZStack(alignment: .bottom) {
+          ZStack(alignment: .bottomTrailing) {
+            ScrollView {
+              VStack(spacing: 24) {
+                CategoryListView(
+                  store: store.scope(
+                    state: \.categoryList,
+                    action: \.categoryList
+                  )
                 )
-              )
-              ArticleListView(
-                store: store.scope(
-                  state: \.articleList,
-                  action: \.articleList
+                ArticleListView(
+                  store: store.scope(
+                    state: \.articleList,
+                    action: \.articleList
+                  )
                 )
-              )
+              }
+              .padding(.bottom, 80)
             }
-            .padding(.bottom, 80)
+            
+            AddFloatingButton {
+              store.send(.floatingButtonTapped)
+            }
+            .padding(.trailing, 20)
+            .padding(.bottom, 24)
           }
           
-          AddFloatingButton {
-            store.send(.floatingButtonTapped)
-          }
-          .padding(.trailing, 20)
-          .padding(.bottom, 24)
-        }
-        
-        if let alertBanner = store.state.alertBanner {
-          AlertBanner(
-            text: alertBanner.text,
-            message: alertBanner.message,
-            style: .close {
-              store.send(.dismissAlertBanner)
+          if let alertBanner = store.state.alertBanner {
+            AlertBanner(
+              text: alertBanner.text,
+              message: alertBanner.message,
+              style: .close {
+                store.send(.dismissAlertBanner)
+              }
+            )
+            .padding(.horizontal, 20)
+            .onTapGesture {
+              store.send(.alertBannerTapped)
             }
-          )
-          .padding(.horizontal, 20)
-          .onTapGesture {
-            store.send(.alertBannerTapped)
           }
         }
       }
