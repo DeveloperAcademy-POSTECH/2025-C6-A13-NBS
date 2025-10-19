451ee9c2efce9ce21e0c337f2edbbfc7356e0e22
Hong <dbsghdz1@gmail.com>
2025-10-19 13:52:59 +0900

    [#26] feat: 카테고리 feature, View 선언

--- Nbs/Projects/Feature/Sources/MyCategory/Feature/CategoryFeature.swift
+++ Nbs/Projects/Feature/Sources/MyCategory/Feature/CategoryFeature.swift
@@ -13,21 +13,32 @@ struct CategoryListFeature {
   struct State: Equatable {
     var categories: [ArticleCategory] = ArticleCategory.mock
     var selectedCategory: ArticleCategory? = ArticleCategory.mock.first
+    var isShowingEmptyView: Bool = false
+    var addLinkView: Bool = false
   }
   
   enum Action {
     case moreCategoryButtonTapped
     case categoryTapped(ArticleCategory)
+    case delegate(Delegate)
+    
+    enum Delegate {
+      case goToMoreLinkButtonView
+    }
   }
   
   var body: some ReducerOf<Self> {
     Reduce { state, action in
       switch action {
       case .moreCategoryButtonTapped:
+        state.isShowingEmptyView = true
         return .none
         
       case let .categoryTapped(category):
         state.selectedCategory = category
+        return .none
+        
+      case .delegate:
         return .none
       }
     }
