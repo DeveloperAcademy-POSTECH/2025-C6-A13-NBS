f3a91628d49b798be260e31940f09321cf7e3855
Hong <dbsghdz1@gmail.com>
2025-10-19 14:23:55 +0900

    [#26] feat: AddLink네임스페이스 지정

---
+++ Nbs/Projects/Feature/Sources/AddLink/Namespace/AddLinkNamespace.swift
@@ -0,0 +1,15 @@
+//
+//  AddLinkNamespace.swift
+//  Feature
+//
+//  Created by 홍 on 10/19/25.
+//
+
+enum AddLinkNamespace {
+  static let naviTitle = "링크 추가하기"
+  static let addLink = "추가할 링크"
+  static let placeHoler = "링크를 입력해주세요"
+  static let selectCategory = "카테고리 선택"
+  static let newCategory = "새 카테고리"
+  static let count = "개"
+}
