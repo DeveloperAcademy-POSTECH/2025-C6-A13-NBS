3a612b0495f9d3030eeb8a22b704e7d677a16691
Hong <dbsghdz1@gmail.com>
2025-10-19 14:24:35 +0900

    [#26] feat: AddLinkFeature 수정

--- Nbs/Projects/Feature/Sources/AddLink/Feature/AddLinkFeature.swift
+++ Nbs/Projects/Feature/Sources/AddLink/Feature/AddLinkFeature.swift
@@ -12,19 +12,29 @@ import SwiftUI
 @Reducer
 struct AddLinkFeature {
   
+  @Dependency(\.dismiss) var dismiss
+  
   @ObservableState
   struct State {
-    
+    var topAppBar = TopAppBarDefaultRightIconxFeature.State(title: "링크 추가하기")
   }
   
   enum Action {
-    
+    case topAppBar(TopAppBarDefaultRightIconxFeature.Action)
   }
   
   var body: some ReducerOf<Self> {
+    Scope(state: \.topAppBar, action: \.topAppBar) {
+      TopAppBarDefaultRightIconxFeature()
+    }
+    
     Reduce { state, action in
       switch action {
+      case .topAppBar(.tapBackButton):
+        return .run { _ in await self.dismiss() }
         
+      case .topAppBar:
+        return .none
       }
     }
   }
