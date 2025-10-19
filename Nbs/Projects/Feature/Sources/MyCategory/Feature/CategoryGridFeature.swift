d927be4044d8be5e62f06efafb3faade6ea1b762
Hong <dbsghdz1@gmail.com>
2025-10-19 22:52:17 +0900

    [#26] feat: 카테고리 Grid추가

--- Nbs/Projects/Feature/Sources/MyCategory/Feature/CategoryGridFeature.swift
+++ Nbs/Projects/Feature/Sources/MyCategory/Feature/CategoryGridFeature.swift
@@ -7,14 +7,36 @@
 
 import ComposableArchitecture
 
+import Domain
+
 @Reducer
 struct CategoryGridFeature {
-  struct State: Equatable {}
-  enum Action {}
+  struct State: Equatable {
+    var categories: [CategoryItem] = []
+  }
+  enum Action {
+    case onAppear
+    case fetchCategoriesResponse(TaskResult<[CategoryItem]>)
+  }
+  
+  @Dependency(\.swiftDataClient) var swiftDataClient
   
   var body: some ReducerOf<Self> {
     Reduce { state, action in
-      return .none
+      switch action {
+      case .onAppear:
+        return .run { send in
+          await send(.fetchCategoriesResponse(TaskResult {
+            try swiftDataClient.fetchCategories()
+          }))
+        }
+      case let .fetchCategoriesResponse(.success(categories)):
+        state.categories = categories
+        return .none
+      case .fetchCategoriesResponse(.failure):
+        // Handle error if needed
+        return .none
+      }
     }
   }
 }
--- Nbs/Projects/Feature/Sources/MyCategory/View/CategoryGridView.swift
+++ Nbs/Projects/Feature/Sources/MyCategory/View/CategoryGridView.swift
@@ -12,10 +12,33 @@ import DesignSystem
 
 struct CategoryGridView {
   let store: StoreOf<CategoryGridFeature>
+  private let gridItems: [GridItem] = [
+    .init(.flexible(), spacing: 10),
+    .init(.flexible(), spacing: 10)
+  ]
 }
 
 extension CategoryGridView: View {
   var body: some View {
-    Text("안녕하세요")
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      LazyVGrid(columns: gridItems, spacing: 10) {
+        ForEach(viewStore.categories) { category in
+          Button {
+            print("")
+          } label: {
+            Text(category.categoryName)
+              .font(.B1_SB)
+              .foregroundStyle(.text1)
+              .frame(maxWidth: .infinity, minHeight: 50)
+              .background(Color.brown)
+              .cornerRadius(10)
+          }
+        }
+      }
+      .padding(.horizontal, 20)
+      .onAppear {
+        viewStore.send(.onAppear)
+      }
+    }
   }
 }
