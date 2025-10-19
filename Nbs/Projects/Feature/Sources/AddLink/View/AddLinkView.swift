e7c5720cc5cc19e0357ba437dfb314938f20a613
Hong <dbsghdz1@gmail.com>
2025-10-19 14:24:16 +0900

    [#26] feat: AddLinkView생성

--- Nbs/Projects/Feature/Sources/AddLink/View/AddLinkView.swift
+++ Nbs/Projects/Feature/Sources/AddLink/View/AddLinkView.swift
@@ -10,10 +10,30 @@ import SwiftUI
 import ComposableArchitecture
 
 struct AddLinkView: View {
-    
-    let store: StoreOf<AddLinkFeature>
-    
-    var body: some View {
-        Text("Add Link View")
+  
+  let store: StoreOf<AddLinkFeature>
+  
+  var body: some View {
+    VStack {
+      TopAppBarDefaultRightIconxFeatureView(
+        store: store.scope(
+          state: \.topAppBar,
+          action: \.topAppBar
+        )
+      )
+      
+      Text(AddLinkNamespace.addLink)
+        .font(.B2_SB)
+        .foregroundStyle(.caption1)
+      
+      Spacer()
     }
+    .navigationBarHidden(true)
+  }
+}
+
+#Preview {
+  AddLinkView(store: Store(initialState: AddLinkFeature.State()) {
+    AddLinkFeature()
+  })
 }
