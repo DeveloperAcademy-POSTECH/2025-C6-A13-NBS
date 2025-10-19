1a32a2caaeba51248efb28b62480e0e004c48d4a
Hong <dbsghdz1@gmail.com>
2025-10-19 22:07:39 +0900

    [#26] feat: 카테고리 추가 로직

--- Nbs/Projects/Feature/Sources/MyCategory/Feature/AddCategoryFeature.swift
+++ Nbs/Projects/Feature/Sources/MyCategory/Feature/AddCategoryFeature.swift
@@ -14,29 +14,28 @@ struct AddCategoryFeature {
     case binding(BindingAction<State>)
     case saveButtonTapped
     case cancelButtonTapped
-    case delegate(Delegate)
-    
-    enum Delegate {
-      case categorySaved(CategoryItem)
-    }
   }
   
   @Dependency(\.dismiss) var dismiss
+  @Dependency(\.swiftDataClient) var swiftDataClient
   
   var body: some ReducerOf<Self> {
     BindingReducer()
     Reduce { state, action in
       switch action {
       case .saveButtonTapped:
-        let newCategory = CategoryItem(categoryName: state.categoryName, icon: state.selectedIcon)
+        let newCategory = CategoryItem(
+          categoryName: state.categoryName,
+          icon: state.selectedIcon
+        )
         return .run {
           send in
-          await send(.delegate(.categorySaved(newCategory)))
+          try await swiftDataClient.addCategory(newCategory)
           await self.dismiss()
         }
       case .cancelButtonTapped:
         return .run { _ in await self.dismiss() }
-      case .binding, .delegate:
+      case .binding:
         return .none
       }
     }
