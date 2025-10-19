4b3045887e5384968aaa2069ef8ba8be874054ab
Hong <dbsghdz1@gmail.com>
2025-10-19 16:52:32 +0900

    [#26] feat: 스위프트 데이터 클라이언트 추가

---
+++ Nbs/Projects/Feature/Sources/SwiftDataClient.swift
@@ -0,0 +1,38 @@
+
+import Foundation
+import SwiftData
+import ComposableArchitecture
+
+import Domain
+
+struct SwiftDataClient {
+  var fetchCategories: () throws -> [CategoryItem]
+  var addCategory: (CategoryItem) throws -> Void
+}
+
+extension SwiftDataClient: DependencyKey {
+  static let liveValue: Self = {
+    guard let modelContainer = AppGroupContainer.createShareModelContainer() else {
+      fatalError("Failed to create shared ModelContainer.")
+    }
+    let modelContext = ModelContext(modelContainer)
+    
+    return Self(
+      fetchCategories: {
+        let descriptor = FetchDescriptor<CategoryItem>()
+        return try modelContext.fetch(descriptor)
+      },
+      addCategory: { category in
+        modelContext.insert(category)
+        try modelContext.save()
+      }
+    )
+  }()
+}
+
+extension DependencyValues {
+  var swiftDataClient: SwiftDataClient {
+    get { self[SwiftDataClient.self] }
+    set { self[SwiftDataClient.self] = newValue }
+  }
+}
