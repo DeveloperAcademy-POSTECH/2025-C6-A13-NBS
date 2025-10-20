2c25deab450c2eef33eb4fa8d7f831681319d278
Seunge E <sjin3687@gmail.com>
2025-10-21 00:21:05 +0900

    [#40] feat: categoryChip SwiftData로 변경

--- Nbs/Projects/Feature/Sources/LinkList/Reducer/CategoryChipFeature.swift
+++ Nbs/Projects/Feature/Sources/LinkList/Reducer/CategoryChipFeature.swift
@@ -6,27 +6,49 @@
 //
 
 import ComposableArchitecture
+import Domain
 
 @Reducer
 struct CategoryChipFeature {
+  @Dependency(\.swiftDataClient) var swiftDataClient
+  
   @ObservableState
   struct State: Equatable {
     /// 선택 가능한 카테고리 목록
-    var categories: [String] = [
-      "전체", "시사 스터디", "경제", "금융", "기술", "디자인", "안녕", "나는", "집에", "가고", "싶어"
-    ]
+    var categories: [CategoryItem] = []
     
     /// 현재 선택된 카테고리
-    var selectedCategory: String = "전체"
+    var selectedCategory: CategoryItem? = nil
   }
   
   enum Action {
-    case categoryTapped(String)
+    case onAppear
+    case categoriesResponse(TaskResult<[CategoryItem]>)
+    case categoryTapped(CategoryItem)
   }
   
   var body: some ReducerOf<Self> {
     Reduce { state, action in
       switch action {
+      case .onAppear:
+        /// 카테고리 불러오기
+        return .run { send in
+          await send(.categoriesResponse(TaskResult {
+            try swiftDataClient.fetchCategories()
+          }))
+        }
+        
+      case let .categoriesResponse(.success(items)):
+        /// 카테고리를 가상의 첫 항목으로 추가
+        let allCategory = CategoryItem(categoryName: "전체", icon: .init(number: 0))
+        state.categories = [allCategory] + items
+        state.selectedCategory = allCategory
+        return .none
+        
+      case let .categoriesResponse(.failure(error)):
+        print("Category fetch failed:", error)
+        return .none
+        
       case let .categoryTapped(category):
         state.selectedCategory = category
         return .none
