a494c76c1464da3b9f2e8b9e22b92c48cc9a8310
Hong <dbsghdz1@gmail.com>
2025-10-21 19:48:34 +0900

    [#26] feat: EditCategoryView 생성

--- Nbs/Projects/Feature/Sources/MyCategory/View/EditCategoryView.swift
+++ Nbs/Projects/Feature/Sources/MyCategory/View/EditCategoryView.swift
@@ -5,3 +5,48 @@
 //  Created by 홍 on 10/21/25.
 //
 
+import SwiftUI
+
+import ComposableArchitecture
+import DesignSystem
+
+struct EditCategoryView {
+  let store: StoreOf<EditCategoryFeature>
+}
+
+extension EditCategoryView: View {
+  var body: some View {
+    VStack {
+      TopAppBarTitleOnly(title: store.naviTitle)
+      CategoryGridView(
+        store: store.scope(
+          state: \.categoryGrid,
+          action: \.categoryGrid
+        )
+      )
+      HStack {
+        MainButton(
+          "취소",
+          style: .soft
+        ) {
+          store.send(.cancelButtonTapped)
+        }
+        MainButton(
+          "수정하기",
+          isDisabled: store.selectedCategory == nil
+        ) {
+          print("수정하기")
+        }
+      }
+      .padding(.horizontal, 20)
+    }
+    .background(DesignSystemAsset.background.swiftUIColor)
+    .toolbar(.hidden)
+  }
+}
+
+#Preview {
+  EditCategoryView(store: Store(initialState: EditCategoryFeature.State()) {
+    EditCategoryFeature()
+  })
+}
