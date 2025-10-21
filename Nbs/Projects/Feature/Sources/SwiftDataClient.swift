fda96e0666b8721bae431f1237bbed80fc4ec28d
YeoSeongil <seongil_yeo@naver.com>
2025-10-21 14:12:54 +0900

    [#36] feat: SwiftDataClient 수정
    검색 기능 추가

--- Nbs/Projects/Feature/Sources/SwiftDataClient.swift
+++ Nbs/Projects/Feature/Sources/SwiftDataClient.swift
@@ -6,10 +6,14 @@ import ComposableArchitecture
 import Domain
 
 struct SwiftDataClient {
+  // LinkItem
+  var fetchLinks: () throws -> [LinkItem]
+  var searchLinks: (String) throws -> [LinkItem]
+  var addLink: (LinkItem) throws -> Void
+  
+  // CategoryItem
   var fetchCategories: () throws -> [CategoryItem]
   var addCategory: (CategoryItem) throws -> Void
-  var addLink: (LinkItem) throws -> Void
-  var fetchLinkItem: () throws -> [LinkItem]
 }
 
 extension SwiftDataClient: DependencyKey {
@@ -18,6 +22,21 @@ extension SwiftDataClient: DependencyKey {
     let modelContext = ModelContext(modelContainer)
     
     return Self(
+      fetchLinks: {
+        let descriptor = FetchDescriptor<LinkItem>()
+        return try modelContext.fetch(descriptor)
+      },
+      searchLinks: { query in
+        let predicate = #Predicate<LinkItem> {
+          $0.title.contains(query)
+        }
+        let descriptor = FetchDescriptor<LinkItem>(predicate: predicate)
+        return try modelContext.fetch(descriptor)
+      },
+      addLink: { link in
+        modelContext.insert(link)
+        try modelContext.save()
+      },
       fetchCategories: {
         let descriptor = FetchDescriptor<CategoryItem>()
         return try modelContext.fetch(descriptor)
@@ -25,14 +44,6 @@ extension SwiftDataClient: DependencyKey {
       addCategory: { category in
         modelContext.insert(category)
         try modelContext.save()
-      },
-      addLink: { link in
-        modelContext.insert(link)
-        try modelContext.save()
-      },
-      fetchLinkItem: {
-        let descriptor = FetchDescriptor<LinkItem>()
-        return try modelContext.fetch(descriptor)
       }
     )
   }()
