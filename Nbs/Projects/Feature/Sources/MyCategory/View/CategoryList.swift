1cb58bc74be9ce326f2caf41deb0cd658a19673f
Hong <dbsghdz1@gmail.com>
2025-10-22 01:48:40 +0900

    [#45] fix: 카테고리 리스트 수정

--- Nbs/Projects/Feature/Sources/MyCategory/View/CategoryList.swift
+++ Nbs/Projects/Feature/Sources/MyCategory/View/CategoryList.swift
@@ -69,47 +69,51 @@ extension CategoryListView: View {
         }
         .padding(.horizontal, 20)
       } else {
-        ScrollView(.horizontal, showsIndicators: false) {
-          HStack(spacing: 8) {
-            ForEach(store.categories.reversed()) { category in
-              Button {
-                store.send(.categoryTapped(category))
-              } label: {
-                ZStack(alignment: .bottomTrailing) {
-                  VStack(alignment: .leading, spacing: 4) {
-                    Text(category.categoryName)
-                      .font(.B1_SB)
-                      .foregroundStyle(.text1)
-                      .multilineTextAlignment(.leading)
-                      .lineLimit(2)
-                    Text("\(category.links.count)개")
-                      .font(.B2_M)
-                      .foregroundStyle(.caption1)
-                    Spacer()
-                  }
-                  .frame(maxWidth: .infinity, alignment: .leading)
-                  .padding(.top, 16)
-                  .padding(.leading, 16)
-                  Image(uiImage: DesignSystemAsset.emptyImage.image)
-                    .resizable()
-                    .frame(width: 56, height: 56)
-                    .padding(.trailing, 12)
-                    .padding(.bottom, 12)
-                }
-                .frame(maxWidth: .infinity, minHeight: 116)
-                .background(Color.blue)
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-              }
-              .frame(width: 160, height: 116)
-              .background(store.selectedCategory == category ? Color.blue : Color.gray.opacity(0.2))
-              .cornerRadius(12)
-            }
-          }
-          .padding(.horizontal, 20)
-        }
+        scrollView
       }
     }
     .onAppear { store.send(.onAppear) }
+  }
+  
+  private var scrollView: some View {
+    ScrollView(.horizontal, showsIndicators: false) {
+      HStack(spacing: 8) {
+        ForEach(store.categories.reversed()) { category in
+          Button {
+            store.send(.categoryTapped(category))
+          } label: {
+            ZStack(alignment: .bottomTrailing) {
+              VStack(alignment: .leading, spacing: 4) {
+                Text(category.categoryName)
+                  .font(.B1_SB)
+                  .foregroundStyle(.text1)
+                  .multilineTextAlignment(.leading)
+                  .lineLimit(2)
+                Text("\(category.links.count)개")
+                  .font(.B2_M)
+                  .foregroundStyle(.caption1)
+                Spacer()
+              }
+              .frame(maxWidth: .infinity, alignment: .leading)
+              .padding(.top, 16)
+              .padding(.leading, 16)
+              DesignSystemAsset.categoryIcon(number: category.icon.number)
+                .resizable()
+                .frame(width: 56, height: 56)
+                .padding(.trailing, 12)
+                .padding(.bottom, 12)
+            }
+            .frame(maxWidth: .infinity, minHeight: 116)
+            .background(DesignSystemAsset.color(number: category.icon.number))
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+          }
+          .frame(width: 160, height: 116)
+          .background(store.selectedCategory == category ? Color.blue : Color.gray.opacity(0.2))
+          .cornerRadius(12)
+        }
+      }
+      .padding(.horizontal, 20)
+    }
   }
 }
 
