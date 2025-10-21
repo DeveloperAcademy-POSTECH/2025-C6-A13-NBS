1f29c7b0e9a93e75efa8984c5f63f0e4e6a4a539
Hong <dbsghdz1@gmail.com>
2025-10-22 00:47:48 +0900

    [#45] fix: 플로팅 버튼 그림자 추가

--- Nbs/Projects/Feature/Sources/Home/View/AddFloatingView.swift
+++ Nbs/Projects/Feature/Sources/Home/View/AddFloatingView.swift
@@ -20,6 +20,12 @@ struct AddFloatingButton: View {
         .frame(width: 48, height: 48)
         .background(.n0)
         .clipShape(Circle())
+        .overlay(
+          Circle()
+            .stroke(.divider1, lineWidth: 1)
+        )
+        .shadow(color: .bgShadow1, radius: 3, x: 0, y: 2)
+        .shadow(color: .bgShadow2, radius: 2, x: 0, y: 2)
     }
   }
 }
