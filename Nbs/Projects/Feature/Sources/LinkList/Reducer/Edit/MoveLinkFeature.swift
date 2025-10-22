836add6b5a51aa17defe49471a30089815d6cf34
Seunge E <sjin3687@gmail.com>
2025-10-23 03:06:31 +0900

    [#43] feat: MoveLinkFeature 구현

--- Nbs/Projects/Feature/Sources/LinkList/Reducer/Edit/MoveLinkFeature.swift
+++ Nbs/Projects/Feature/Sources/LinkList/Reducer/Edit/MoveLinkFeature.swift
@@ -14,14 +14,14 @@ struct MoveLinkFeature {
   @ObservableState
   struct State: Equatable {
     var allLinks: [LinkItem] = []
-    var selectedLinks: Set<LinkItem.ID> = []
+    var selectedLinks: Set<String> = []
     var isSelectAll: Bool = false
   }
   
-  enum Action {
+  enum Action: BindableAction {
+    case binding(BindingAction<State>)
     case onAppear
     case toggleSelect(LinkItem)
-    case toggleSelectAll
     case cancelTapped
     case confirmMoveTapped
     case delegate(Delegate)
@@ -33,28 +33,29 @@ struct MoveLinkFeature {
   }
   
   var body: some ReducerOf<Self> {
+    BindingReducer()
     Reduce { state, action in
       switch action {
       case .onAppear:
         return .none
         
+        /// 전체 선택 or 해제
+      case .binding(\.isSelectAll):
+        if state.isSelectAll {
+          state.selectedLinks = Set(state.allLinks.map(\.id))
+        } else {
+          state.selectedLinks.removeAll()
+        }
+        return .none
+        
+        /// 개별 토글 시 전체선택 여부 갱신
       case let .toggleSelect(link):
         if state.selectedLinks.contains(link.id) {
           state.selectedLinks.remove(link.id)
         } else {
           state.selectedLinks.insert(link.id)
         }
-        state.isSelectAll = (state.selectedLinks.count == state.allLinks.count)
-        return .none
-        
-      case .toggleSelectAll:
-        if state.isSelectAll {
-          state.selectedLinks.removeAll()
-          state.isSelectAll = false
-        } else {
-          state.selectedLinks = Set(state.allLinks.map(\.id))
-          state.isSelectAll = true
-        }
+        state.isSelectAll = state.selectedLinks.count == state.allLinks.count
         return .none
         
       case .cancelTapped:
@@ -64,7 +65,7 @@ struct MoveLinkFeature {
         let selected = state.allLinks.filter { state.selectedLinks.contains($0.id) }
         return .send(.delegate(.confirmMove(selected: selected)))
         
-      case .delegate:
+      case .binding, .delegate:
         return .none
       }
     }
