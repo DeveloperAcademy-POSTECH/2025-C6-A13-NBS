dd47ee5dd84357745ed1bd59057ca37fda347391
Hong <dbsghdz1@gmail.com>
2025-10-26 23:55:41 +0900

    [#67] feat: AppRoute추가

---
+++ Nbs/Projects/App/Sources/Navigation/AppRouterGroup.swift
@@ -0,0 +1,26 @@
+//
+//  AppRouterGroup.swift
+//  Nbs
+//
+//  Created by 홍 on 10/26/25.
+//
+
+import Feature
+import LinkNavigator
+
+public struct AppRouterGroup {
+  public init() { }
+}
+
+extension AppRouterGroup {
+
+  @MainActor
+  func routers() -> [RouteBuilderOf<SingleLinkNavigator>] {
+    [
+      HomeRouteBuilder().generate(),
+      AddLinkRouteBuilder().generate(),
+      MyCategoryRouteBuilder().generate(),
+      AddCategoryRouteBuilder().generate()
+    ]
+  }
+}
