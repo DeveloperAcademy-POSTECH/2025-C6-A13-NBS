6f5b2832955cd7ec80f8bcb670dfcb4bb7a9c0f9
Hong <dbsghdz1@gmail.com>
2025-10-26 23:58:26 +0900

    [#67] fix: HomeEntry뷰 수정

--- Nbs/Projects/Feature/Sources/Home/View/HomeEntryView.swift
+++ Nbs/Projects/Feature/Sources/Home/View/HomeEntryView.swift
@@ -7,18 +7,25 @@
 //
 
 import SwiftUI
+
 import ComposableArchitecture
+import LinkNavigator
 
 /// App 모듈과 같이 외부 모듈에서 Home 기능을 사용하기 위한 진입점(Entry Point) 역할을 하는 View입니다.
 /// 내부적으로 HomeFeature의 Store를 생성하고 관리하여, 외부에서는 복잡한 초기화 과정 없이 사용할 수 있습니다.
 public struct HomeEntryView: View {
-  private let store = Store(initialState: HomeFeature.State()) {
-    HomeFeature()
+  private let store: StoreOf<HomeFeature>
+  let navigator: SingleLinkNavigator
+  
+  public init(navigator: SingleLinkNavigator) {
+    self.navigator = navigator
+    self.store = Store(initialState: HomeFeature.State()) {
+      HomeFeature()
+        .dependency(\.linkNavigator, .init(navigator: navigator))
+    }
   }
   
-  public init() {}
-  
   public var body: some View {
-    HomeView(store: store)
+    HomeView(navigator: navigator, store: store)
   }
 }
