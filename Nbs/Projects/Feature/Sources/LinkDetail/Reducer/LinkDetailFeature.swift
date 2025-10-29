2b89c9465fddc8e185d307873b6274355fb63902
Seunge E <sjin3687@gmail.com>
2025-10-29 18:10:37 +0900

    [#87] feat: 기사 제목 수정 기능 구현
    LinkDetailFeature

--- Nbs/Projects/Feature/Sources/LinkDetail/Reducer/LinkDetailFeature.swift
+++ Nbs/Projects/Feature/Sources/LinkDetail/Reducer/LinkDetailFeature.swift
@@ -14,13 +14,18 @@ struct LinkDetailFeature {
   
   @ObservableState
   struct State: Equatable {
-//    var id: String { link.id }
     var link: ArticleItem
+    var isEditingTitle = false
+    var editedTitle = ""
   }
   
   enum Action {
     case onAppear
-    case refreshHighlights
+    case editButtonTapped
+    case titleChanged(String)
+    case titleFocusChanged(Bool)
+    case saveIfNeeded
+    case saveResponse(TaskResult<Void>)
   }
   
   var body: some ReducerOf<Self> {
@@ -29,7 +34,43 @@ struct LinkDetailFeature {
       case .onAppear:
         return .none
         
-      case .refreshHighlights:
+      case .editButtonTapped:
+        state.isEditingTitle = true
+        state.editedTitle = state.link.title
+        return .none
+        
+      case .titleChanged(let text):
+        state.editedTitle = text
+        return .none
+        
+      case .titleFocusChanged(false): // 포커스 해제 시
+        let current = state.link.title.trimmingCharacters(in: .whitespacesAndNewlines)
+        let next = state.editedTitle.trimmingCharacters(in: .whitespacesAndNewlines)
+        state.isEditingTitle = false
+        if !next.isEmpty, next != current {
+          return .send(.saveIfNeeded)
+        }
+        return .none
+        
+      case .titleFocusChanged(true):
+        return .none
+        
+      case .saveIfNeeded:
+        let id = state.link.id
+        let newTitle = state.editedTitle.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard !newTitle.isEmpty, newTitle != state.link.title else { return .none }
+        return .run { send in
+          await send(.saveResponse(TaskResult {
+            try swiftDataClient.editLinkTitle(id, newTitle)
+          }))
+        }
+        
+      case .saveResponse(.success):
+        state.link.title = state.editedTitle.trimmingCharacters(in: .whitespacesAndNewlines)
+        return .none
+        
+      case .saveResponse(.failure(let error)):
+        print("❌ 제목 수정 실패:", error)
         return .none
       }
     }
