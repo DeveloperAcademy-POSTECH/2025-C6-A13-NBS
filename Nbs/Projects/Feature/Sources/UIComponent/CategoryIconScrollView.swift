068e5cfd56ed4cffef73e4a4c9ae013a87370d1b
Hong <dbsghdz1@gmail.com>
2025-11-06 20:02:20 +0900

    [#82] feat: 카테고리 아이콘 스크롤 컴포넌트화

---
+++ Nbs/Projects/Feature/Sources/UIComponent/CategoryIconScrollView.swift
@@ -0,0 +1,67 @@
+import SwiftUI
+
+import ComposableArchitecture
+import DesignSystem
+import Domain
+
+struct CategoryIconScrollView {
+  @Binding var selectedIcon: CategoryIcon?
+  let isFocused: Bool
+  
+  let columns = [
+    GridItem(.flexible(), spacing: 10),
+    GridItem(.flexible(), spacing: 10),
+    GridItem(.flexible(), spacing: 10)
+  ]
+}
+
+extension CategoryIconScrollView: View {
+  var body: some View {
+    VStack {
+      Text("카테고리 아이콘")
+        .font(.B2_SB)
+        .foregroundStyle(.caption1)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.leading, 24)
+        .padding(.top, 24)
+      
+      ScrollView {
+        LazyVGrid(columns: columns, spacing: 16) {
+          ForEach(1..<16, id: \.self) { index in
+            let isSelected = selectedIcon?.number == index
+            Button {
+              selectedIcon = .init(number: index)
+            } label: {
+              RoundedRectangle(cornerRadius: 12)
+                .fill(
+                  isSelected
+                  ? DesignSystemAsset.bl1.swiftUIColor
+                  : DesignSystemAsset.n0.swiftUIColor
+                )
+                .overlay(
+                  RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(
+                      isSelected
+                      ? DesignSystemAsset.bl6.swiftUIColor
+                      : Color.clear,
+                      lineWidth: 1.25
+                    )
+                )
+                .aspectRatio(1, contentMode: .fit)
+                .overlay(
+                  DesignSystemAsset.primaryCategoryIcon(number: index)
+                    .resizable()
+                    .frame(width: 56, height: 56)
+                )
+            }
+            .shadow(color: .bgShadow3, radius: 4, x: 0, y: 0)
+            .buttonStyle(.plain)
+            .disabled(isFocused)
+          }
+        }
+        .padding(.horizontal, 20)
+      }
+      .scrollDisabled(isFocused)
+    }
+  }
+}
