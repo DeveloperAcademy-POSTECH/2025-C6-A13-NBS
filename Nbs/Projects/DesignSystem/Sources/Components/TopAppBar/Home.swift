2b4c6ba4ea5255bd747435f5d8fbb62bb76be6ce
Seunge E <sjin3687@gmail.com>
2025-11-06 18:30:38 +0900

    [#93] fix: 아이콘 크기 수정

--- Nbs/Projects/DesignSystem/Sources/Components/TopAppBar/Home.swift
+++ Nbs/Projects/DesignSystem/Sources/Components/TopAppBar/Home.swift
@@ -59,7 +59,6 @@ extension TopAppBarHome: View {
             .resizable()
             .renderingMode(.template)
             .foregroundStyle(.icon)
-            .frame(width: 24, height: 24)
             .frame(width: 44, height: 44)
         }
       }
