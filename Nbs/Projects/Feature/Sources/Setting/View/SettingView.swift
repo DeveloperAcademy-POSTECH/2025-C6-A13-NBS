08b947075e93ed62fb3590cd656d9f4c063e40f0
Seunge E <sjin3687@gmail.com>
2025-11-06 14:21:58 +0900

    [#108] feat: 설정 메인 View 구현

---
+++ Nbs/Projects/Feature/Sources/Setting/View/SettingView.swift
@@ -0,0 +1,127 @@
+//
+//  SettingView.swift
+//  Feature
+//
+//  Created by 이안 on 11/5/25.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+import DesignSystem
+import Domain
+
+/// 앱 메인 설정뷰
+struct SettingView: View {
+  let store: StoreOf<SettingFeature>
+}
+
+// MARK: View
+extension SettingView {
+  var body: some View {
+    ZStack {
+      Color.background.ignoresSafeArea()
+      
+      VStack {
+        navigationBar
+        scrollView
+      }
+    }
+    .navigationBarHidden(true)
+  }
+  
+  private var navigationBar: some View {
+    TopAppBarDefaultRightIconx(title: "설정") {
+      store.send(.backButtonTapped)
+    }
+  }
+  
+  private var scrollView: some View {
+    ScrollView(.vertical, showsIndicators: false) {
+      topContents
+      
+      Rectangle()
+        .fill(.divider1)
+        .frame(height: 1)
+      
+      bottomContents
+    }
+    .padding(.horizontal, 20)
+  }
+  
+  private var topContents: some View {
+    VStack(alignment: .leading, spacing: 10) {
+      Text("알아두면 좋은 팁")
+        .font(.B2_M)
+        .foregroundStyle(.caption1)
+        .multilineTextAlignment(.leading)
+        .frame(height: 32)
+      
+      tipGrid
+    }
+    .padding(.bottom, 34)
+  }
+  
+  private var tipGrid: some View {
+    LazyVGrid(
+      columns: [
+        GridItem(.flexible(), spacing: 12),
+        GridItem(.flexible(), spacing: 12)
+      ],
+      spacing: 12
+    ) {
+      SettingTipCard(icon: Icon.alertCircle, title: "Safari extension 허용하기") {
+        //        store.send(.safariTipTapped)
+      }
+      
+      SettingTipCard(icon: Icon.book, title: "하이라이트 및 메모 \n연습하기") {
+        //        store.send(.highlightTipTapped)
+      }
+      
+      SettingTipCard(icon: Icon.share, title: "우리앱으로 손쉽게 \n공유하기") {
+        //        store.send(.shareTipTapped)
+      }
+      
+      SettingTipCard(icon: Icon.heart, title: "즐겨찾기 설정하기") {
+        //        store.send(.favoriteTipTapped)
+      }
+    }
+  }
+  
+  private var bottomContents: some View {
+    VStack(alignment: .leading, spacing: .zero) {
+      Text("정보")
+        .font(.B2_M)
+        .foregroundStyle(.caption1)
+        .multilineTextAlignment(.leading)
+        .frame(height: 32)
+      
+      infoList
+    }
+    .padding(.top, 24)
+  }
+  
+  private var infoList: some View {
+    VStack(spacing: .zero) {
+      InfoListItem(icon: Icon.info, title: "앱 버전", trailing: .text(Constants.appVersion))
+      
+      InfoListItem(icon: Icon.shield, title: "개인정보 처리방침", trailing: .chevron) {
+        store.send(.privacyPolicyTapped)
+      }
+      
+      InfoListItem(icon: Icon.file, title: "서비스 이용약관", trailing: .chevron) {
+        store.send(.termsOfServiceTapped)
+      }
+      
+      InfoListItem(icon: Icon.opensource, title: "사용된 오픈 소스", trailing: .chevron) {
+        store.send(.openSourceTapped)
+      }
+    }
+  }
+}
+
+#Preview {
+  SettingView(store: Store(initialState: SettingFeature.State(), reducer: {
+    SettingFeature()
+  }))
+}
