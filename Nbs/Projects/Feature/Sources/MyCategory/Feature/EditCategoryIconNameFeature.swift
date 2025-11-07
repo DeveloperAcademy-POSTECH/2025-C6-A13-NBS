f9525c1a39ef49915505246c99cb9b45cef7de94
Hong <dbsghdz1@gmail.com>
2025-11-07 11:34:49 +0900

    [#82] fix: 카테고리 텍스트필드 errorCaption적용

--- Nbs/Projects/Feature/Sources/MyCategory/Feature/EditCategoryIconNameFeature.swift
+++ Nbs/Projects/Feature/Sources/MyCategory/Feature/EditCategoryIconNameFeature.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 import ComposableArchitecture
 import Domain
+import DesignSystem
 
 @Reducer
 struct EditCategoryIconNameFeature {
@@ -23,6 +24,8 @@ struct EditCategoryIconNameFeature {
     var categoryName: String
     var category: CategoryItem?
     var selectedIcon: CategoryIcon?
+    var isDuplicate: Bool = false
+    var textFieldStyle: JNTextFieldStyle = .default
     
     init(category: CategoryItem?) {
       self.category = category
@@ -36,6 +39,8 @@ struct EditCategoryIconNameFeature {
     case topAppBar(TopAppBarDefaultRightIconxFeature.Action)
     case setCategoryName(String)
     case selectIcon(CategoryIcon?)
+    case _setDuplicate(Bool)
+    case setTextFieldStyle(JNTextFieldStyle)
   }
   
   var body: some ReducerOf<Self> {
@@ -47,22 +52,44 @@ struct EditCategoryIconNameFeature {
       switch action {
       case let .setCategoryName(name):
         state.categoryName = name
+        state.isDuplicate = false
+        state.textFieldStyle = .default
         return .none
       case let .selectIcon(icon):
         state.selectedIcon = icon
         return .none
       case .compeleteButtonTapped:
-        return .run { [id = state.category?.id, name = state.categoryName, icon = state.selectedIcon] _ in
-          guard let id, let icon else { return }
-          await MainActor.run {
-            do {
-              try swiftDataClient.updateCategoryItem(id, name, icon)
-            } catch {
-              print("카테고리 업데이트 실패 \(error)")
-            }
-          }
-          await linkNavigator.pop()
+        return .run { [category = state.category, name = state.categoryName] send in
+          let categories = try swiftDataClient.fetchCategories()
+          let isDuplicate = categories.contains { $0.categoryName.lowercased() == name.lowercased() && $0.id != category?.id }
+          
+          await send(._setDuplicate(isDuplicate))
         }
+        
+      case let ._setDuplicate(isDuplicate):
+        state.isDuplicate = isDuplicate
+        state.textFieldStyle = isDuplicate ? .errorCaption : .default
+        
+        if !isDuplicate {
+          return .run { [id = state.category?.id, name = state.categoryName, icon = state.selectedIcon] _ in
+            guard let id, let icon else { return }
+            await MainActor.run {
+              do {
+                try swiftDataClient.updateCategoryItem(id, name, icon)
+              } catch {
+                print("카테고리 업데이트 실패 \(error)")
+              }
+            }
+            await linkNavigator.pop()
+          }
+        } else {
+          return .none
+        }
+        
+        case let .setTextFieldStyle(style):
+          state.textFieldStyle = style
+          return .none
+          
       case .topAppBar(.tapBackButton):
         return .run { _ in await linkNavigator.pop() }
       }
