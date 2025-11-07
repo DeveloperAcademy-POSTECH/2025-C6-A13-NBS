92d7111beb63b60913102f589f2350b0d823970d
Hong <dbsghdz1@gmail.com>
2025-11-07 11:35:50 +0900

    [#82] feat: UI변경, 키보드추가

--- Nbs/Projects/Feature/Sources/MyCategory/View/EditCategoryIconNameView.swift
+++ Nbs/Projects/Feature/Sources/MyCategory/View/EditCategoryIconNameView.swift
@@ -29,16 +29,24 @@ extension EditCategoryIconNameView: View {
       TopAppBarDefaultRightIconx(title: "카테고리 수정하기") {
         store.send(.topAppBar(.tapBackButton))
       }
+      
       JNTextField(
-        text: $store.categoryName.sending(
-          \.setCategoryName
-        ),
-        style: .default,
+        text: $store.categoryName.sending(\.setCategoryName),
+        style: $store.textFieldStyle.sending(\.setTextFieldStyle),
         placeholder: "카테고리명을 입력해주세요",
+        caption: "이미 존재하는 카테고리예요",
         header: "카테고리명"
       )
       .focused($isFocused)
-
+      .toolbar {
+        ToolbarItemGroup(placement: .keyboard) {
+          Spacer()
+          Button("완료") {
+            isFocused = false
+          }
+        }
+      }
+      
       CategoryIconScrollView(
         selectedIcon: $store.selectedIcon.sending(\.selectIcon),
         isFocused: isFocused
@@ -52,14 +60,10 @@ extension EditCategoryIconNameView: View {
         store.send(.compeleteButtonTapped)
       }
     }
-    .contentShape(Rectangle())
+    .background(DesignSystemAsset.background.swiftUIColor)
     .onTapGesture {
       isFocused = false
     }
-    .onTapGesture {
-      isFocused = false
-    }
-    .background(Color.background)
     .toolbar(.hidden)
     .ignoresSafeArea(.keyboard)
   }
