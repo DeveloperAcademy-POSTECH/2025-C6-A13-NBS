0c3c2480429b98df73f8530d301f701b203adaa5
Hong <dbsghdz1@gmail.com>
2025-11-08 13:00:32 +0900

    [#113] feat: 온보딩 서비스화면 추가
    온보딩 첫 화면

---
+++ Nbs/Projects/App/Sources/Onboarding/Feature/OnboardingServiceFeature.swift
@@ -0,0 +1,33 @@
+//
+//  OnboardingServiceFeature.swift
+//  Nbs
+//
+//  Created by 홍 on 11/7/25.
+//
+
+import ComposableArchitecture
+import LinkNavigator
+
+@Reducer
+struct OnboardingServiceFeature {
+  @Dependency(\.linkNavigator) var navigation
+  
+  @ObservableState
+  struct State {
+    
+  }
+  
+  enum Action {
+    case startButtonTapped
+  }
+  
+  var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case .startButtonTapped:
+        navigation.push(.onboarding, nil)
+        return .none
+      }
+    }
+  }
+}
