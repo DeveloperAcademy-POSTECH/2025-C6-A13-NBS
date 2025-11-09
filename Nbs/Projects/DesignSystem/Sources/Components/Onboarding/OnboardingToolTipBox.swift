cbce7bc8c9c6c6cbf904053cd4ef2d7992ddde0f
Hong <dbsghdz1@gmail.com>
2025-11-09 12:14:23 +0900

    [#113] feat: 온보딩 툴박스 추가

--- Nbs/Projects/DesignSystem/Sources/Components/Onboarding/OnboardingToolTipBox.swift
+++ Nbs/Projects/DesignSystem/Sources/Components/Onboarding/OnboardingToolTipBox.swift
@@ -19,10 +19,10 @@ extension OnboardingToolTipBox: View {
   public var body: some View {
     VStack(spacing: 0) {
       
-        Triangle()
-          .rotation(.degrees(180))
-          .fill(.bl6)
-          .frame(width: 16, height: 10)
+      Triangle()
+        .rotation(.degrees(180))
+        .fill(.bl6)
+        .frame(width: 16, height: 10)
       
       Text(text)
         .font(.B1_M_HL)
@@ -34,9 +34,9 @@ extension OnboardingToolTipBox: View {
             .fill(.bl6)
         )
       
-//      Triangle()
-//        .fill(.bl6)
-//        .frame(width: 16, height: 10)
+      //      Triangle()
+      //        .fill(.bl6)
+      //        .frame(width: 16, height: 10)
     }
   }
 }
@@ -52,12 +52,6 @@ fileprivate struct Triangle: Shape {
     return path
   }
 }
-
-//#Preview(body: {
-//  OnboardingToolTipBox(text: "하이라이트 치는 방법을 배워볼게요")
-//})
-
-import SwiftUI
 
 public struct OnboardingToolTipBoxBottom {
   let text: String
@@ -84,4 +78,69 @@ extension OnboardingToolTipBoxBottom: View {
         .frame(width: 16, height: 10)
     }
   }
+}
+
+public struct OnboardingToolTipBoxBottomTrailing {
+  let text: String
+  
+  public init(text: String) {
+    self.text = text
+  }
+}
+
+extension OnboardingToolTipBoxBottomTrailing: View {
+  public var body: some View {
+    VStack(spacing: 0) {
+      Text(text)
+        .font(.B1_M_HL)
+        .foregroundStyle(.textw)
+        .padding(.horizontal, 14)
+        .padding(.vertical, 6)
+        .background(
+          RoundedRectangle(cornerRadius: 4)
+            .fill(.bl6)
+        )
+        .overlay(alignment: .bottomTrailing) {
+          Triangle()
+            .fill(.bl6)
+            .frame(width: 18, height: 12)
+            .padding(.trailing, 14)
+            .padding(.bottom, -15)
+        }
+    }
+  }
+}
+
+public struct OnboardingToolTipBoxTopLeading {
+  let text: String
+  
+  public init(text: String) {
+    self.text = text
+  }
+}
+
+extension OnboardingToolTipBoxTopLeading: View {
+  public var body: some View {
+    VStack(alignment: .leading, spacing: 0) {
+      Triangle()
+        .rotation(.degrees(180))
+        .fill(.bl6)
+        .frame(width: 16, height: 10)
+        .padding(.leading, 14) // 텍스트 padding과 정렬 포인트! ✨
+
+      Text(text)
+        .font(.B1_M_HL)
+        .foregroundStyle(.textw)
+        .padding(.horizontal, 14)
+        .padding(.vertical, 6)
+        .background(
+          RoundedRectangle(cornerRadius: 4)
+            .fill(.bl6)
+        )
+    }
+  }
+}
+
+#Preview {
+  OnboardingToolTipBoxTopLeading(text: "g하이g하이g하이g하이g하이")
 }
