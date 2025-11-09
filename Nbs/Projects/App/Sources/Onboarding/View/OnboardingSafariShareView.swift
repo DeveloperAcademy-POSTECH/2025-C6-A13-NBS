c7f91a604a2058cffa3631173c08e186aed9b890
Hong <dbsghdz1@gmail.com>
2025-11-09 12:44:23 +0900

    [#113] feat: 사파리 쉐어 뷰 추가

---
+++ Nbs/Projects/App/Sources/Onboarding/View/OnboardingSafariShareView.swift
@@ -0,0 +1,55 @@
+//
+//  OnboardingSafariShareView.swift
+//  Nbs
+//
+//  Created by 홍 on 11/9/25.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+import DesignSystem
+
+struct OnboardingSafariShareView {
+  let store: StoreOf<OnboardingSafariShareFeature>
+}
+
+extension OnboardingSafariShareView: View {
+  var body: some View {
+    ZStack(alignment: .bottom) {
+      VStack(spacing: 0) {
+        TopAppBarDefaultRightIconx(title: "") {
+          store.send(.backButtonTapped)
+        }
+        OnboardingTitleImage(
+          title: .highlightMemoTitle,
+          description: .highlightMemoDescription,
+          image: DesignSystemAsset.onboardingService.swiftUIImage,
+          showPage: true,
+          currentPage: store.currentPage
+        )
+        .padding(.top, 16)
+      }
+      
+      VStack {
+        MainButton("저장 방법 알아보기", hasGradient: true) {
+          store.send(.nextButtonTapped)
+        }
+        .buttonStyle(.plain)
+        
+        Button(action: {
+          store.send(.skipButtonTapped)
+        }) {
+          Text("건너뛰기")
+            .font(.C2)
+            .foregroundStyle(.caption2)
+            .underline()
+        }
+        .padding(.top, 8)
+      }
+      .background(Color.background)
+    }
+    .background(Color.background)
+    .toolbar(.hidden)
+  }
+}
