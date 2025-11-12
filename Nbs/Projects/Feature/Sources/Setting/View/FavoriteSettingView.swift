dbdd7707b68626fd3d93a78e2af9f7137df8f5ea
Seunge E <sjin3687@gmail.com>
2025-11-12 17:54:15 +0900

    [#123] feat: 즐겨찾기 설정하기 뷰 구현

---
+++ Nbs/Projects/Feature/Sources/Setting/View/FavoriteSettingView.swift
@@ -0,0 +1,63 @@
+//
+//  FavoriteSettingView.swift
+//  Feature
+//
+//  Created by 이안 on 11/12/25.
+//
+
+import SwiftUI
+import AVKit
+
+import ComposableArchitecture
+import DesignSystem
+
+struct FavoriteSettingView: View {
+  let store: StoreOf<FavoriteSettingFeature>
+  @State private var isReady = false
+}
+
+extension FavoriteSettingView {
+  var body: some View {
+    ZStack {
+      Color.background.ignoresSafeArea()
+      VStack(spacing: 16) {
+        TopAppBarDefaultRightIconx(title: "즐겨찾기 설정하기") {
+          store.send(.backButtonTapped)
+        }
+        
+        VStack(spacing: 30) {
+          Text("탭탭을 즐겨찾기 설정하여\n더 쉽게 공유할 수 있어요! ")
+            .font(.B1_M)
+            .foregroundStyle(.caption1)
+          
+          ZStack {
+            Color.background
+              .cornerRadius(32)
+              .opacity(isReady ? 0 : 1)
+      
+            CustomVideoView(
+              url: URL(string: AppConfig.settingFavoriteURL)!,
+              onReady: { isReady = true }
+            )
+            .opacity(isReady ? 1: 0)
+            .cornerRadius(32)
+            .clipped()
+            .overlay {
+              RoundedRectangle(cornerRadius: 32)
+                .stroke(Color.divider2, lineWidth: 3)
+            }
+            .padding(14)
+          }
+        }
+        .padding(.horizontal, 52)
+        
+      }
+    }
+  }
+}
+
+#Preview {
+  FavoriteSettingView(store: Store(initialState: FavoriteSettingFeature.State(), reducer: {
+    FavoriteSettingFeature()
+  }))
+}
