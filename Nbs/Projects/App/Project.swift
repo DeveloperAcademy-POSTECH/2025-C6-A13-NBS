45c671e11c4a61882e69305abd65241a0902f6ae
Hong <dbsghdz1@gmail.com>
2025-12-08 23:15:01 +0900

    [#170] fix: 중복처리된 인증서 수정

--- Nbs/Projects/App/Project.swift
+++ Nbs/Projects/App/Project.swift
@@ -50,42 +50,21 @@ let appTarget = Target.target(
 
 let safariTarget = Target.target(
   name: TargetName.SafariExtension.rawValue,
-  destinations: .iOS,
   product: .appExtension,
   bundleId: Project.bundleID + ".app.safariExtension",
-  deploymentTargets: .iOS("17.0"),
   infoPlist: .file(path: "SafariExtension/info.plist"),
   sources: [SourceFileGlob(stringLiteral: TargetName.SafariExtension.sourcesPath)],
   resources: [ResourceFileElement(stringLiteral: TargetName.SafariExtension.resourcesPath)],
   entitlements: .file(path: "SafariExtension.entitlements"),
   dependencies: [
     .domain()
-  ],
-  settings: .settings(
-    base: [
-      "DEVELOPMENT_TEAM": "WN2B884S76",
-      "CODE_SIGN_STYLE": "Manual"
-    ],
-    configurations: [
-      .debug(name: "Debug", settings: [
-        "PRODUCT_BUNDLE_IDENTIFIER": "com.Nbs.dev.app.safariExtension",
-        "CODE_SIGN_IDENTITY": "Apple Development: Yunhong Kim (Q7CMJ86WZQ)",
-        "PROVISIONING_PROFILE_SPECIFIER": "com.Nbs.dev.app.safariExtension Development"
-      ]),
-      .release(name: "Release", settings: [
-        "CODE_SIGN_IDENTITY": "Apple Distribution: Yunhong Kim (WN2B884S76)",
-        "PROVISIONING_PROFILE_SPECIFIER": "match AppStore com.Nbs.dev.ADA.app.safariExtension"
-      ])
-    ]
-  )
+  ]
 )
 
 let shareExtensionTarget = Target.target(
   name: TargetName.ShareExtension.rawValue,
-  destinations: .iOS,
   product: .appExtension,
   bundleId: Project.bundleID + ".app.shareExtension",
-  deploymentTargets: .iOS("17.0"),
   infoPlist: .file(path: "ShareExtension/info.plist"),
   sources: [SourceFileGlob(stringLiteral: TargetName.ShareExtension.sourcesPath)],
   resources: [ResourceFileElement(stringLiteral: TargetName.ShareExtension.resourcesPath)],
@@ -94,25 +73,7 @@ let shareExtensionTarget = Target.target(
     .sdk(name: "UniformTypeIdentifiers", type: .framework),
     .designSystem(),
     .domain()
-  ],
-  settings: .settings(
-    base: [
-      "DEVELOPMENT_TEAM": "WN2B884S76",
-      "CODE_SIGN_STYLE": "Manual",
-      "TARGETED_DEVICE_FAMILY": "1,2"
-    ],
-    configurations: [
-      .debug(name: "Debug", settings: [
-        "PRODUCT_BUNDLE_IDENTIFIER": "com.Nbs.dev.app.shareExtension",
-        "CODE_SIGN_IDENTITY": "Apple Development: Yunhong Kim (Q7CMJ86WZQ)",
-        "PROVISIONING_PROFILE_SPECIFIER": "com.Nbs.dev.app.shareExtension Development"
-      ]),
-      .release(name: "Release", settings: [
-        "CODE_SIGN_IDENTITY": "Apple Distribution: Yunhong Kim (WN2B884S76)",
-        "PROVISIONING_PROFILE_SPECIFIER": "match AppStore com.Nbs.dev.ADA.app.actionExtension"
-      ])
-    ]
-  )
+  ]
 )
 
 let project = Project.project(
